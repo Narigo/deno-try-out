0c03dac6b0e35c99a1eee6849f06840a20f8761c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-07-20 16:42:09 +0200

    Check read permissions
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/hello.ts
+++ src/hello.ts
@@ -1,9 +1,17 @@
 import { readFileStr } from "./read-file.ts";
 
 (async () => {
-  const content = await readFileStr("./hello.txt");
-  
-  console.log(content);
+  try {
+    const content = await readFileStr("./hello.txt");
+    const content2 = await readFileStr("./hello2.txt");
+    const contentAgain = await readFileStr("./hello.txt");
+
+    console.log(content);
+    console.log(content2);
+    console.log(contentAgain);
+  } catch (error) {
+    console.log("there was an error!", error);
+  }
 })();
 
 // import relatively
